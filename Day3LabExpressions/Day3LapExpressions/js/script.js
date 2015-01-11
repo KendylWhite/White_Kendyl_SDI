1b7c4e1b4cbcb6353b57611b823e2cf766475ead
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-10 23:51:09 -0500

    Day 3 Lab
    Slice of pie part 1

---
+++ Day3LabExpressions/Day3LapExpressions/js/script.js
@@ -0,0 +1,14 @@
+/*
+Kendyl White
+1/10/2015
+Day 3 Lab
+ */
+
+//Slice of pie part 1
+var slicesPerPizza = 8;
+var peopleAtTheParty = 10;
+var pizzasOrdered = 5;
+
+//multiply slices per pizza * pizzas ordered / people at the party
+var numberOfSlicesPerPerson = slicesPerPizza * pizzasOrdered / peopleAtTheParty
+console.log("Each person ate " + numberOfSlicesPerPerson + " slices of pizza at the party.");
