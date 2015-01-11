5a43afa6daf22b041719b7fbb402b947574ce443
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-10 23:56:34 -0500

    Day 3 Lab
    I made a change to part 1.

--- Day3LabExpressions/Day3LapExpressions/js/script.js
+++ Day3LabExpressions/Day3LapExpressions/js/script.js
@@ -6,9 +6,11 @@ Day 3 Lab
 
 //Slice of pie part 1
 var slicesPerPizza = 8;
-var peopleAtTheParty = 10;
-var pizzasOrdered = 5;
+var peopleAtTheParty = 22;
+var pizzasOrdered = 11;
 
 //multiply slices per pizza * pizzas ordered / people at the party
 var numberOfSlicesPerPerson = slicesPerPizza * pizzasOrdered / peopleAtTheParty
 console.log("Each person ate " + numberOfSlicesPerPerson + " slices of pizza at the party.");
+
+
