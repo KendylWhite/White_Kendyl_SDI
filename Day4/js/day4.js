c708cf053bedcd27efabe1ac43e03e1c97caf169
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-13 19:06:19 -0500

    Advanced Arrays
    Learned methods

--- Day4/js/day4.js
+++ Day4/js/day4.js
@@ -45,4 +45,57 @@ avengersNames[avengersNames.length] = "Powerman";
 console.log(avengersNames);
 
 
+//Pick oranges and track how many we have
+
+var orangeBins = [12, 20, 14];
+
+//How many pins of oranges did we pick in total
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log("The total number of orange bins collected is " + total);
+
+
+//Advanced Array
+
+var fruitBowl = ["Orange", "Tomato", "Strawberry"];
+
+//Property - Length - How many items are in the array
+console.log(fruitBowl.length + " items in our fruit bowl.");
+
+//Method of Arrays
+
+//push - insert an item at the end of our array
+//arrayName.push(item to push)
+
+fruitBowl.push("Apple");
+
+//see fruit bowl
+console.log(fruitBowl);
+
+//push another fruit
+fruitBowl.push("Kiwi");
+console.log(fruitBowl);
+
+//pop - takes off the last item in an array AND returns it
+//arrayName.pop()
+//Create a variable to "catch" the return value
+var caught = fruitBowl.pop();
+console.log(fruitBowl);
+console.log(caught);
+var caught2 = fruitBowl.pop();
+console.log(fruitBowl);
+console.log(caught2);
+
+//splice - used fro removing or adding items into an existing array
+//arrayName.splice(index # position to start from, # of items to remove, items to put in)
+fruitBowl.splice(2, 0, "Banana");
+console.log(fruitBowl);
+
+fruitBowl.splice(1, 1, "Kiwi");
+console.log(fruitBowl);
+
+//Adding more items
+fruitBowl.splice(1, 0, "Tangerine", "Babboa", "Grapes");
+console.log(fruitBowl);
+
+
 
