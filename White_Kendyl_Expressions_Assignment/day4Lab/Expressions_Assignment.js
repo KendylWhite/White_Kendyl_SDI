fd247836cc47fe3805ac3926722eb9837dcd2706
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-15 15:58:42 -0500

    Expressions Assignment
    Foundation

---
+++ White_Kendyl_Expressions_Assignment/day4Lab/Expressions_Assignment.js
@@ -0,0 +1,14 @@
+
+ /*
+Kendyl White
+SDI Campus
+1/13/2015
+Expressions Assignment
+ */
+//alert("Testing to see if it works");
+
+//The volume of a Pyramid
+
+ var length = prompt("The Giza Pyramid is located in Egypt. It has a volume of 2.6 million cubic meters. You may not have one that big, but we can still find the volume. \nEnter the length:");
+ var width = prompt("Next, please enter the width of your pyramid:");
+ var height = prompt("Last, but not least, enter the height and you'll have your volume.");
