7f4393a66b1c1c546f9072608169d7c82bce4826
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-15 18:55:30 -0500

    Basic if statement
    Will it run or not

--- Day5/js/day5Conditional.js
+++ Day5/js/day5Conditional.js
@@ -7,4 +7,28 @@ Conditionals
  */
 //alert("Testing to see if it works");
 
+ //Basic Conditional Statement
+
+ //Create a boolean variable to test
+ var oldEnough = true;
+
+ //If the child is old enough, then print to the console, You Can Ride!
+
+ /*
+ Basic Structure of if statement
+
+ if(condition to test){
+ Code to run if the condition is true
+ }
+
+  */
+
+ if(oldEnough){
+  //Code inside of {} will run if the test is true
+  //This code will be skipped if test is false
+  console.log("You can ride the coaster!");
+ }
+
+ console.log("What comes after the if statement.");
+
  
