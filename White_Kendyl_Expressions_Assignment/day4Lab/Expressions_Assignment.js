f5a5703e2f7b0500814ab3449686d3fb4c438d10
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-15 16:08:58 -0500

    Expressions Assignment
    Formula

--- White_Kendyl_Expressions_Assignment/day4Lab/Expressions_Assignment.js
+++ White_Kendyl_Expressions_Assignment/day4Lab/Expressions_Assignment.js
@@ -12,3 +12,6 @@ Expressions Assignment
  var length = prompt("The Giza Pyramid is located in Egypt. It has a volume of 2.6 million cubic meters. You may not have one that big, but we can still find the volume. \nEnter the length:");
  var width = prompt("Next, please enter the width of your pyramid:");
  var height = prompt("Last, but not least, enter the height and you'll have your volume.");
+
+ var volumeOfPyramid = (length * width * height) / 3;
+ console.log(volumeOfPyramid);
