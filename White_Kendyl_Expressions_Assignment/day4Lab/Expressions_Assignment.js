a6354b45b03ce9441e7712db709b03e92723cc5f
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-15 22:56:31 -0500

    Expressions Assignment
    Almost there!

--- White_Kendyl_Expressions_Assignment/day4Lab/Expressions_Assignment.js
+++ White_Kendyl_Expressions_Assignment/day4Lab/Expressions_Assignment.js
@@ -8,10 +8,18 @@ Expressions Assignment
 //alert("Testing to see if it works");
 
 //The volume of a Pyramid
+ var volume = ["length" , "width" ,"height"];
 
+//Prompting the user
  var length = prompt("The Giza Pyramid is located in Egypt. It has a volume of 2.6 million cubic meters. You may not have one that big, but we can still find the volume. \nEnter the length:");
  var width = prompt("Next, please enter the width of your pyramid:");
  var height = prompt("Last, but not least, enter the height and you'll have your volume.");
 
- var volumeOfPyramid = (length * width * height) / 3;
- console.log(volumeOfPyramid);
+//Using Assignment Operators
+ var baseOfPyramid = length * width;
+ var preVolume = baseOfPyramid *= length;
+
+//The Final Volume of the Pyramid
+ var volumeOfPyramid = preVolume /= 3;
+ console.log("The volume of your pyramid with a " + volume[0] + " of " + length + ", a " + volume[1] + " of " + width + ", and a " + volume[2] + " of " + height + " is " + volumeOfPyramid + ". Now apply your cubic unit if you have one.");
+
