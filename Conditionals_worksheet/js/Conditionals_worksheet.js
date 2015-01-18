b7ef9b6104a850c483a1d976d3eb9e5cd0c045ce
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-17 22:45:08 -0500

    Conditionals Worksheet
    Grade Letter Calculator

--- Conditionals_worksheet/js/Conditionals_worksheet.js
+++ Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -20,3 +20,35 @@ Conditionals Worksheet
  } else {
   console.log("You only have " + gallonsLeft + " gallons of gas left in your tank, better get gas now while you can!");
  }
+
+//Grade Letter Calculator
+ //Grading system
+ // A+ - 95 - 100
+ // A - 90 - 94
+ // B+ - 85 - 89
+ // B - 80 - 84
+ // C+ - 76 - 79
+ // C - 73 - 75
+ // D - 70 - 72
+ // F - 0 - 69
+
+ var studentGrade = 96;
+
+ if (studentGrade >= 95 && studentGrade <= 100){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned an A+ in the class!");
+ } else if (studentGrade >= 90 && studentGrade <= 94){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned an A in the class!")
+ } else if (studentGrade >= 85 && studentGrade <= 89){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned a B+ in the class!")
+ } else if (studentGrade >= 80 && studentGrade <= 84){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned a B in the class!")
+ } else if (studentGrade >= 76 && studentGrade <= 79){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned a C+ in the class!")
+ } else if (studentGrade >= 73 && studentGrade <= 75){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned a C in the class!")
+ } else if (studentGrade >= 70 && studentGrade <= 72){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned a D in the class!")
+ }else if (studentGrade >= 0 && studentGrade <= 69){
+  console.log("You have earned a " + studentGrade + "%, which means you have earned an F in the class!")
+ }
+ 
