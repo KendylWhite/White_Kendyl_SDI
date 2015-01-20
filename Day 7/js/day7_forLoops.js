bb54ece67acc8bcdc97b85c337adc825ac0a1fb0
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-20 18:50:44 -0500

    Learned about for loops and break command
    dito

--- Day 7/js/day7_forLoops.js
+++ Day 7/js/day7_forLoops.js
@@ -4,4 +4,33 @@
  *For Loops
  *
  */
-alert("Testing");
+//alert("Testing");
+
+
+//Baisx Fpr Loop Structure
+/*
+for(initialization; Condition To Test: Increment Of Change){
+    
+    Code to run as long as the condtion is true
+}
+
+*/
+
+for(var i = 0; i < 20; i += 4){
+    //Code to run as long as i < 20
+    console.log("The value of i = " + i);
+    
+}
+
+
+for(var k = 0; k < 5; k ++){
+    
+    console.log("The value of k is " + k + ".");
+    //Add a break point
+    //If k is equal to 3 then stop the code
+    
+    if (k === 3) {
+        //code
+        break;
+    }
+}
