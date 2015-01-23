1e57db22147825d80f6087dac15718ed7b6a2003
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-22 23:03:19 -0500

    Functions Worksheet
    Circumference found!

--- Functions_worksheet/js/Functions_worksheet.js
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -5,3 +5,18 @@
  */
 //alert("Testing");
 
+//Circumference
+//Calculate the circfumference of a circle
+function calcCirc(radius) {
+    
+    var circ = Math.PI * (radius * radius);
+    console.log("The circumference of a circle with a radius of " + radius + " is rounded to be " + circ.toFixed(2) + ".");
+    
+}
+
+//Function call
+calcCirc(6);
+calcCirc(3);
+
+var userCirc = parseInt(prompt("Let's find the circumference of your circle! \nPlease enter the radius of your cirlce:"));
+calcCirc(userCirc);
