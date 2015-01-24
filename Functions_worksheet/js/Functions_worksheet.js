53011610cc7faaa79e337f95e415ef67ecf9534a
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-24 04:55:25 -0500

    Functions Worksheet
    Done!

--- Functions_worksheet/js/Functions_worksheet.js
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -12,6 +12,9 @@ function calcCirc(radius) {
     var circ = Math.PI * (radius * radius);
     console.log("The circumference of a circle with a radius of " + radius + " is rounded to be " + circ.toFixed(2) + ".");
     
+//Return
+    return circ
+    
 }
 
 //Function call
@@ -20,3 +23,21 @@ calcCirc(3);
 
 var userCirc = parseInt(prompt("Let's find the circumference of your circle! \nPlease enter the radius of your cirlce:"));
 calcCirc(userCirc);
+
+//Sting!
+//Calculate the number of stings it takes to kill an animal
+function calcStings(pounds) {
+    
+    var stings = 8.67 * pounds;
+    console.log("It takes " + stings + " stings to kill this animal.")
+    
+//Return
+    return stings
+
+}
+
+//Funtion call
+calcStings(22);
+
+var userStings = parseInt(prompt("I'm pretty sure you care about your animal's safety and bees are everywhere. It takes about 8.67 stings per pound to kill an animal. \nHow much does your pet weigh?"));
+calcStings(userStings);
