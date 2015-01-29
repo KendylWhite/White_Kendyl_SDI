f8ab7e87d792ce7b2cbe49b4c0ca9924842c41e2
KendylWhite <Kendyl.White.322@gmail.com>
2015-01-29 18:49:30 -0500

    Final Exam
    Prompted the user the price.

--- Final Exam/js/finalExam.js
+++ Final Exam/js/finalExam.js
@@ -5,3 +5,11 @@ SDI Campus
 Final Exam WPF
  */
 //alert("Testing to see if it works");
+
+//Create a file thar calculates a discounted sales price
+//Prompt the user the cost of the item.
+var price = prompt("I see that you're trying to buy something and you want the discounted price. \nWhat is the cost of the item:");
+while(price === "" || isNaN(price)){
+ var price = prompt("Please don't leave blank and only use numbers. \nWhat is the cost of the item:");
+}
+
